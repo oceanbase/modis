3083ce179e607a484251100ec05e3d6e6f32eefc
wumengjie.wmj <wumengjie.wmj@oceanbase.com>
2024-06-07 16:01:34 +0800

    fix err msg not valid float

--- command/strings.go
+++ command/strings.go
@@ -18,6 +18,7 @@ package command
 
 import (
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/oceanbase/obkv-table-client-go/table"
@@ -227,7 +228,11 @@ func IncrByFloat(ctx *CmdContext) error {
 
 	f64, err := ctx.CodecCtx.DB.Storage.IncrByFloat(ctx.CodecCtx.DB.Ctx, ctx.CodecCtx.DB.ID, key, value)
 	if err != nil {
-		ctx.OutContent = resp.EncError("ERR " + err.Error())
+		if strings.Contains(err.Error(), "-5114") {
+			ctx.OutContent = resp.EncError("ERR value is not a valid float")
+		} else {
+			ctx.OutContent = resp.EncError("ERR " + err.Error())
+		}
 	} else {
 		ctx.OutContent = resp.EncBulkString(strconv.FormatFloat(f64, 'f', -1, 64))
 	}
